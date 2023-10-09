b1a22eae82b576118bf23fd1ec42cf866dd715fb
Tamirlan667 <99018335+Tamirlan667@users.noreply.github.com>
2023-10-09 13:45:36 +0600

    sdsdsd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import AppInfo from "./components/app-info/app-info";
+import SearchPanel from "./components/search-panel/search-panel";
+import AppFilter from "./components/app-filter/app-filter";
+import EmployersList from "./components/employers-list/employers-list";
+import EmployersAddForm from "./components/employers-add-form/employers-add-form";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <AppInfo />
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+      <EmployersList />
+      <EmployersAddForm />
     </div>
   );
 }
